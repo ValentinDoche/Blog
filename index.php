68a32f28e2f048c76911992fdb33bfb7b8781531
Valentin Doche <contact@valentindoche.com>
2019-10-27 00:55:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <!doctype html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>VALENTIN DOCHE | BLOG </title>
-    <link rel="stylesheet" href="assets/css/materialize.css"> <!-- ONLY USED FOR AUTOCOMPLETE -->
-    <link rel="stylesheet" href="assets/css/main.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://cdn.rawgit.com/Dogfalo/materialize/master/dist/js/materialize.min.js"></script>
+    <?php
+    require 'themes/default/head.php'
+    ?>
 </head>
 <body>
 <?php
