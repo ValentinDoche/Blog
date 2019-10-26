89efec9e3f87d26400779dc4f01da2b543fb6144
Valentin Doche <contact@valentindoche.com>
2019-10-27 00:55:17 +0100

    Create head.php

---
+++ themes/default/head.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="assets/css/materialize.css"> <!-- ONLY USED FOR AUTOCOMPLETE -->
+<link rel="stylesheet" href="assets/css/main.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://cdn.rawgit.com/Dogfalo/materialize/master/dist/js/materialize.min.js"></script>
