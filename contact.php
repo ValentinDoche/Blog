e7fed0fe9b9107aeb44588ddb677bf952b8fad18
Valentin Doche <contact@valentindoche.com>
2019-10-27 17:42:40 +0000

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <title>VALENTIN DOCHE | BLOG </title>
+    <?php
+    require 'themes/default/head.php'
+    ?>
+</head>
+<body>
+<?php
+require 'themes/default/header.php';
+require 'themes/default/contact.php';
+require 'themes/default/footer.php';
+?>
+
+</body>
+</html>
