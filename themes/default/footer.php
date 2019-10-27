3e7a405ddcbec4a90f27052d5b1e447b73beb554
Valentin Doche <contact@valentindoche.com>
2019-10-27 11:38:54 +0000

    Update footer.php

--- themes/default/footer.php
+++ themes/default/footer.php
@@ -1,4 +1,4 @@
-<footer class="page-footer" style="padding-top: 0">
+<footer class="page-footer" style="padding-top: 0; z-index: 99">
     <div class="footer-copyright">
         <div class="container center">
             &copy; All rights reserved <?php echo date('Y') ?> - <a href="https://github.com/valentindoche" target="_blank" class="white-text">Valentin DOCHE</a>  - <a href="https://next-u.eu/" target="_blank" class="white-text">NEXT-U</a>
