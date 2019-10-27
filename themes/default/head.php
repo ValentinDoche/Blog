f5d592417eba65f762d5736b699a0925fd9093f1
Valentin Doche <contact@valentindoche.com>
2019-10-27 11:38:57 +0000

    Update head.php

--- themes/default/head.php
+++ themes/default/head.php
@@ -1,5 +1,11 @@
+<meta charset="UTF-8">
+<meta name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="assets/css/materialize.css"> <!-- ONLY USED FOR AUTOCOMPLETE -->
 <link rel="stylesheet" href="assets/css/main.css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://cdn.rawgit.com/Dogfalo/materialize/master/dist/js/materialize.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.js"></script>
+<!--
+<script src="https://cdn.rawgit.com/Dogfalo/materialize/master/dist/js/materialize.min.js"></script> -->
