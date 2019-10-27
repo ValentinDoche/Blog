10e9ddab3e5bd28bc1069e4f72d30bfdd7bc7d65
Valentin Doche <contact@valentindoche.com>
2019-10-27 17:42:28 +0000

    Create contact.php

---
+++ themes/default/contact.php
@@ -0,0 +1,10 @@
+<main style="margin-top: 1em">
+    <div class="container z-depth-2">
+        <div class="row">
+            <div class="col s12">
+                <h3>ME CONTACTER</h3>
+
+            </div>
+        </div>
+    </div>
+</main>
