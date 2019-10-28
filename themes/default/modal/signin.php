e26527a0b4ab64ea7bc581a7e123f1b014a0d816
Valentin Doche <contact@valentindoche.com>
2019-10-28 18:39:37 +0000

    Update signin.php

--- themes/default/modal/signin.php
+++ themes/default/modal/signin.php
@@ -1,16 +1,16 @@
 <div class="row">
     <div class="col l6 offset-l3 s12">
         <p class="center" style="font-size: 16px; font-weight: bold">WELCOME BACK</p>
-        <form method="post" action="login" name="signin" id="signin">
+        <form method="post" action="/function/signIn.php" name="signin" id="signin">
             <div class="input-field col s12">
                 <i class="material-icons prefix">account_circle</i>
-                <input id="pseudo" type="text" class="validate" required name="pseudo" form="signin">
-                <label for="pseudo">PSEUDO</label>
+                <input id="signin_pseudo" type="text" class="validate" required name="signin_pseudo" form="signin">
+                <label for="signin_pseudo">PSEUDO</label>
             </div>
             <div class="input-field col s12">
                 <i class="material-icons prefix">vpn_key</i>
-                <input id="password" type="password" class="validate" required name="password" form="signin">
-                <label for="password">PASSWORD</label>
+                <input id="signin_password" type="password" class="validate" required name="signin_password" form="signin">
+                <label for="signin_password">PASSWORD</label>
             </div>
             <div class="input-field col s12 center">
                 <button class="btn-large waves-effect waves-light" type="submit" form="signin">SIGN IN <i class="material-icons right">send</i> </button>
