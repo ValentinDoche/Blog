c940b2486a0fd01f6ffa8540543d2c98edb01fc4
Valentin Doche <valentin.doche@hotmail.com>
2019-11-05 11:08:51 +0000

    Update news_articles.php

--- themes/default/modal/news_articles.php
+++ themes/default/modal/news_articles.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col s12">
         <p class="center" style="font-size: 16px; font-weight: bold">NEW ARTICLES</p>
-        <form method="post" action="" id="articles">
+        <form method="post" action="/function/newArticles.php" id="articles">
             <div class="input-field col s12 l6">
                 <i class="material-icons prefix">vpn_key</i>
                 <input id="articles_title" type="text" class="validate" name="articles_title" form="articles">
@@ -9,12 +9,12 @@
             </div>
             <div class="file-field input-field col s12 l6">
                 <div class="btn">
-                    <span>LOGO</span>
-                    <input type="file" name="articles_logo_upload" form="articles">
+                    <span>IMAGE</span>
+                    <input type="file" name="articles_image_upload" form="articles">
                 </div>
                 <div>
                     <div class="file-path-wrapper">
-                        <input class="file-path validate" type="text" id="articles_logo" name="articles_logo" required>
+                        <input class="file-path validate" type="text" id="articles_image" name="articles_image" required>
                     </div>
                 </div>
             </div>
@@ -22,6 +22,9 @@
                 <i class="material-icons prefix">vpn_key</i>
                 <textarea id="articles_content" class="materialize-textarea" required name="articles_content" form="articles" data-length="10000"></textarea>
                 <label for="articles_content">ARTICLES (MARKDOWN ACCEPTED)</label>
+            </div>
+            <div class="input-field col s12 center">
+                <button class="btn-large waves-effect waves-light" type="submit" form="articles">SEND ARTICLE<i class="material-icons right">send</i> </button>
             </div>
         </form>
     </div>
