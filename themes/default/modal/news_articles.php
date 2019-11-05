d47834817606ca933f8416b2367d8a2fc2c8571b
Valentin Doche <valentin.doche@hotmail.com>
2019-11-05 19:29:45 +0000

    Update news_articles.php

--- themes/default/modal/news_articles.php
+++ themes/default/modal/news_articles.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col s12">
         <p class="center" style="font-size: 16px; font-weight: bold">NEW ARTICLES</p>
-        <form method="post" action="/function/newArticles.php" id="articles">
+        <form method="post" action="/function/newArticles.php" id="articles" enctype="multipart/form-data">
             <div class="input-field col s12 l6">
                 <i class="material-icons prefix">title</i>
                 <input id="articles_title" type="text" class="validate" name="articles_title" form="articles" required>
@@ -10,13 +10,18 @@
             <div class="file-field input-field col s12 l6">
                 <div class="btn">
                     <span>IMAGE</span>
-                    <input type="file" name="articles_image_upload" form="articles">
+                    <input type="file" name="articles_image_upload" form="articles" id="articles_image_upload">
                 </div>
                 <div>
                     <div class="file-path-wrapper">
                         <input class="file-path validate" type="text" id="articles_image" name="articles_image" required>
                     </div>
                 </div>
+            </div>
+            <div class="input-field col s12">
+                <i class="material-icons prefix">description</i>
+                <textarea id="articles_summary" class="materialize-textarea" required name="articles_summary" form="articles" data-length="300"></textarea>
+                <label for="articles_summary">description</label>
             </div>
             <div class="input-field col s12">
                 <i class="material-icons prefix">description</i>
@@ -31,7 +36,22 @@
 </div>
 <script>
     $(document).ready(function(){
-        $('#articles_content').characterCounter();
+        $('#articles_content, #articles_summary').characterCounter();
     });
-    M.textareaAutoResize($('#articles_content'));
+    M.textareaAutoResize($('#articles_content, #articles_summary'));
+
+    var uploadField = document.getElementById("articles_image_upload"), file_name = document.getElementById("articles_image");
+
+    uploadField.onchange = function() {
+        if (this.files[0].type == "image/png" || this.files[0].type == "image/jpeg"){
+            if(this.files[0].size > 2097152){
+                file_name.setCustomValidity("Image too big. Max 2mb");
+            }else {
+                file_name.setCustomValidity('');
+            }
+        }else {
+            file_name.setCustomValidity('Supported only .jpeg or .png');
+        }
+        console.log(this.files[0].type)
+    };
 </script>
